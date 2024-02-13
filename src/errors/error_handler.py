f254cba281f65d586a4317ab468b5036197a2dcc
Karen Ribeiro <karenribeiro@MacBook-Air-de-Karen.local>
2024-02-13 19:46:35 -0300

    feat: Implementign Validator

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,7 +1,19 @@
 from src.views.http_types.http_response import HttpResponse
-
+from .error_types.http_unprocessable_entity import HttpUnprocessableEntity
 
 def handler_errors(error:Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntity):
+        # enviar para um log
+        # enviar um email de notificação
+        return HttpResponse(
+            status_code=error.status_code,
+            body={
+                "errors":[{
+                    "title": error.name,
+                    "detail": error.message
+                }]
+            }
+        )
     return HttpResponse(
         status_code=500,
         body={
