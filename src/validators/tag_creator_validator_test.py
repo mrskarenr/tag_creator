0b0570bda63e5cd347c8121b08b7801c5c0882b8
Karen Ribeiro <karenribeiro@MacBook-Air-de-Karen.local>
2024-02-19 13:12:18 -0300

    Initial commit

--- src/validators/tag_creator_validator_test.py
+++ src/validators/tag_creator_validator_test.py
@@ -3,7 +3,7 @@ from .tag_creator_validator import tag_creator_validator
 
 class MockRequest:
     def __init__(self, json) -> None:
-        self.json = json
+        self.json = json 
 def test_tag_creator_validator(): 
     req = MockRequest(json={"product_code": "12345"})
     tag_creator_validator(req)
